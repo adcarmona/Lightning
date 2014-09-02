70c7a7d4aeb54030c18409be3815a8a767ddb9b2
adcarmona <adcarmona98@gmail.com>
2014-09-02 11:30:27 -0700

    Lightning Assignment Completed.

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,52 @@
+int startX = 0;
+int startY = 150;
+int endX = 0;
+int endY = 150;
+int lightningstrokeblue = 50;
+int lightningstrokegreen = 50;
+int lightningstrokered = 50;
+boolean lightning = true;
+
 void setup()
 {
-  size(300,300);
+	size(500,300);
+	background(0);
+	strokeWeight(2);
+	frameRate(20);
 }
 void draw()
 {
-
+	if (lightning == true) {
+	while (endX < 500)
+	{
+		stroke(lightningstrokered,lightningstrokegreen,lightningstrokeblue);
+		endX = startX + int(random(0,10));
+		endY = startY + int(random(-9,10));
+		line(startX,startY,endX,endY);
+		startX = endX;
+		startY = endY;
+		lightningstrokeblue = lightningstrokeblue + 5;
+		lightningstrokegreen = lightningstrokegreen + 5;
+		lightningstrokered = lightningstrokered + 5;
+	}
+	}
+	lightningstrokeblue = 10;
+	lightningstrokegreen = 10;
+	lightningstrokered = 10;
+	startX = 0;
+	startY = 150;
+	endX = 0;
+	endY = 150;
+	fill(0,0,0,50);
+	rect(0,0,500,300);
 }
 void mousePressed()
 {
-
+	if(lightning == true) {
+		lightning = false;
+	}
+	else {
+		lightning = true;
+	}
 }
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,19 +14,56 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+int startX = 0;
+int startY = 150;
+int endX = 0;
+int endY = 150;
+int lightningstrokeblue = 50;
+int lightningstrokegreen = 50;
+int lightningstrokered = 50;
+boolean lightning = true;
+
 public void setup()
 {
-  size(300,300);
-  noLoop();
+	size(500,300);
+	background(0);
+	strokeWeight(2);
+	frameRate(20);
 }
 public void draw()
 {
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+	if (lightning == true) {
+	while (endX < 500)
+	{
+		stroke(lightningstrokered,lightningstrokegreen,lightningstrokeblue);
+		endX = startX + PApplet.parseInt(random(0,10));
+		endY = startY + PApplet.parseInt(random(-9,10));
+		line(startX,startY,endX,endY);
+		startX = endX;
+		startY = endY;
+		lightningstrokeblue = lightningstrokeblue + 5;
+		lightningstrokegreen = lightningstrokegreen + 5;
+		lightningstrokered = lightningstrokered + 5;
+	}
+	}
+	lightningstrokeblue = 10;
+	lightningstrokegreen = 10;
+	lightningstrokered = 10;
+	startX = 0;
+	startY = 150;
+	endX = 0;
+	endY = 150;
+	fill(0,0,0,50);
+	rect(0,0,500,300);
 }
 public void mousePressed()
 {
-  redraw();
+	if(lightning == true) {
+		lightning = false;
+	}
+	else {
+		lightning = true;
+	}
 }
 
   static public void main(String[] passedArgs) {
